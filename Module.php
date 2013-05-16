4ac35a9a2fff7894273a23f500a2a3f8769f9751
Diego Sapriza <AV4TAr@gmail.com>
2013-05-16 16:56:21 -0300

    autoload

--- Module.php
+++ Module.php
@@ -12,10 +12,17 @@ namespace HtmlTable;
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\Mvc\ModuleRouteListener;
 
-class Module implements AutoloaderProviderInterface
-{
+class Module implements AutoloaderProviderInterface {
+    public function getAutoloaderConfig()
+    {
+        return array(
+                'Zend\Loader\StandardAutoloader' => array('namespaces' => array(
+                // if we're in a namespace deeper than one level we need to fix the \ in the path
+                __NAMESPACE__ => __DIR__ . '/src/' . str_replace('\\', '/', __NAMESPACE__),),),);
+    }
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
     }
+
 }
